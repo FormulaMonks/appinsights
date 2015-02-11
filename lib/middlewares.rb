f420f4083d286e654923acd8aa168b6028ec0055
Emiliano Mancuso <emiliano.mancuso@gmail.com>
2015-02-11 15:33:54 -0300

    Handle Middlewares settings

---
+++ lib/middlewares.rb
@@ -0,0 +1,40 @@
+require_relative 'middlewares/exception_handling'
+
+module ApplicationInsightsInstaller
+  class Middlewares
+    class << self
+      def configure(settings = {})
+        @settings = settings || {}
+        @enabled_middlewares = constantize_middlewares
+      end
+
+      def enabled
+        @enabled_middlewares
+      end
+
+      def settings
+        @settings
+      end
+
+      private
+
+      def constantize_middlewares
+        constants = @settings.map do |middleware|
+          begin
+            if middleware['enabled']
+              c = const_get middleware['name']
+              args = middleware['initialize'] || {}
+
+              [c, args]
+            end
+          rescue NameError => e
+            # FIXME: Log, ignore or fail?
+            fail ApplicationInsightsInstaller::UnknownMiddleware.new e
+          end
+        end
+
+        constants.compact.uniq
+      end
+    end
+  end
+end
