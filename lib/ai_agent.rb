78993cb7a83bdbf2bace8f0b278c4812bfadcdb7
Emiliano Mancuso <emiliano.mancuso@gmail.com>
2015-02-11 15:36:45 -0300

    Update gem entry point

--- lib/ai_agent.rb
+++ lib/ai_agent.rb
@@ -1,8 +1,17 @@
-module AIAgent
-  class Railtie < Rails::Railtie
-    initializer 'ai_agent.start_plugin' do |app|
-      require 'pry'; binding.pry
-      AIAgent::Agent.configure app.config
-    end
+require 'toml'
+require_relative 'errors'
+require_relative 'context'
+require_relative 'middlewares'
+require_relative 'config_loader'
+
+module ApplicationInsightsInstaller
+  if defined?(Rails::VERSION)
+    require_relative 'frameworks/rails'
+  else
+    loader = ConfigLoader.new __FILE__
+    # Initialize for other frameworks
+    Context.configure loader.settings['ai']
+    Middlewares.configure loader.settings['middleware']
   end
 end
+
