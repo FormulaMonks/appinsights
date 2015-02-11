89ea1b7639557325eb6aa32042830aef15a01015
Emiliano Mancuso <emiliano.mancuso@gmail.com>
2015-02-11 15:58:00 -0300

    Test Context class

---
+++ test/context_test.rb
@@ -0,0 +1,49 @@
+require_relative 'helper'
+
+describe ApplicationInsightsInstaller::Context do
+  before do
+    @configs = {
+      "instrumentation_key" => "a_key",
+      "custom" => 4,
+      "properties" => "belong to the context",
+      "application" => {
+        "ver" => "0.0.1"
+      },
+      "device" => {
+        "id" => "asdfghjkl1",
+        "os" => "OSX"
+      }
+    }
+  end
+
+  describe 'configure' do
+    it 'returns a ApplicationInsights::Channel::TelemetryContext object' do
+      context = ApplicationInsightsInstaller::Context.configure
+
+      assert_equal ApplicationInsights::Channel::TelemetryContext, context.class
+    end
+
+    it 'accepts a hash to set Context values' do
+      context = ApplicationInsightsInstaller::Context.configure @configs
+
+      assert_equal 'a_key', context.instrumentation_key
+      assert_equal '0.0.1', context.application.ver
+    end
+  end
+
+  describe 'telemetry_client' do
+    before do
+      ApplicationInsightsInstaller::Context.configure @configs
+
+      @client = ApplicationInsightsInstaller::Context.telemetry_client
+    end
+
+    it 'returns an instance of ApplicationInsights::TelemetryClient' do
+      assert_equal ApplicationInsights::TelemetryClient, @client.class
+    end
+
+    it 'sets the context to the Telemetry Client' do
+      assert_equal 'a_key', @client.context.instrumentation_key
+    end
+  end
+end
