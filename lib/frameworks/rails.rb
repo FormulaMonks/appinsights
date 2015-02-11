d98b9380cec95799f59800166a93a3d320333575
Emiliano Mancuso <emiliano.mancuso@gmail.com>
2015-02-11 19:06:01 -0300

    Use Rails.logger when the config file is not found instead of raise an exception.

--- lib/frameworks/rails.rb
+++ lib/frameworks/rails.rb
@@ -1,10 +1,18 @@
 module AppInsights
   class Railtie < Rails::Railtie
     initializer 'ai_agent.start_plugin' do |_app|
-      AppInsights::ConfigLoader.new Rails.root
+      begin
+        AppInsights::ConfigLoader.new Rails.root
 
-      AppInsights::Middlewares.enabled.each do |middleware, args|
-        config.app_middleware.use middleware, *args.values
+        AppInsights::Middlewares.enabled.each do |middleware, args|
+          config.app_middleware.use middleware, *args.values
+        end
+      rescue AppInsights::ConfigFileNotFound => e
+        Rails.logger.error e.message
+        Rails.logger.info <<-EOS
+          Place your config file `application_insights.toml` into your rails
+          application under the `config` directory.
+        EOS
       end
     end
   end
