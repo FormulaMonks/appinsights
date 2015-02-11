48c76fe757c7687bf038650f69b71978f738a7d7
Emiliano Mancuso <emiliano.mancuso@gmail.com>
2015-02-11 15:32:52 -0300

    Define ConfigLoader class

---
+++ lib/config_loader.rb
@@ -0,0 +1,28 @@
+module ApplicationInsightsInstaller
+  class ConfigLoader
+    attr_reader :settings, :filename
+
+    def initialize(root, filename = nil)
+      @root = root
+      @filename = filename || default_file
+
+      fail ApplicationInsightsInstaller::ConfigFileNotFound unless @filename
+
+      @settings = TOML.load_file @filename
+    end
+
+    private
+
+    def default_file
+      default_paths.compact.find { |path| File.exist? path }
+    end
+
+    def default_paths
+      @default_paths ||= [
+        File.join(@root, './config/application_insights.toml'),
+        File.join(@root, './application_insights.toml'),
+        # ENV['AI_CONFIG_PATH']
+      ]
+    end
+  end
+end
